af805ee3401e72addb0fddbfb9e0cb3f24df4203
Dhanush Ekollu <66331423+DEboy2007@users.noreply.github.com>
2026-07-09 01:40:20 -0400

    LC-course-schedule [Runtime: N/A; Memory: N/A]

--- graph/course-schedule.cpp
+++ graph/course-schedule.cpp
@@ -1,37 +1,31 @@
 class Solution {
 public:
-    bool dfs(int node, vector<vector<int>> &graph, vector<int> &visited) {
-        if (visited[node] == 1) {
-            return false;
-        }
-        if (visited[node] == 2) {
-            return true;
-        }
-        visited[node] = 1; // mark visited
-        for (int adj : graph[node]) {
-            if (!dfs(adj, graph, visited)) {
-                return false;
-            }
-        }
-        visited[node] = 2; // mark finished
-        return true;
-    }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> graph(numCourses);
-        for (auto &pair : prerequisites) {
-            graph[pair[1]].push_back(pair[0]); // store course in index of prereq
+        vector<int> indegree(numCourses, 0);
+        vector<vector<int>> adj(numCourses);
+        for (auto& pre : prerequisites) {
+            indegree[pre[1]]++;
+            adj[pre[0]].push_back(pre[1]);
         }
-        vector<int> visited(numCourses, 0);
 
+        queue<int> q;
         for (int i = 0; i < numCourses; i++) {
-            if (visited[i] == 0) {
-                if (!dfs(i, graph, visited)) {
-                    return false;
+            if (indegree[i] == 0) q.push(i);
+        }
+
+        int checked = 0;
+        while (!q.empty()) {
+            int curr = q.front();
+            q.pop();
+            checked++;
+            for (int a : adj[curr]) {
+                indegree[a]--;
+                if (indegree[a] == 0) {
+                    q.push(a);
                 }
             }
         }
-
-        return true;
+        return checked == numCourses;
         
     }
 };
