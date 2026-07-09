c53849d414fd175e9592371bd985bce842d9d982
Dhanush Ekollu <66331423+DEboy2007@users.noreply.github.com>
2026-07-09 00:03:13 -0400

    LC-find-the-duplicate-number [Runtime: N/A; Memory: N/A]

--- bit-manipulation/find-the-duplicate-number.cpp
+++ bit-manipulation/find-the-duplicate-number.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        set<int> seen;
-        for (int i : nums) {
-            if (seen.contains(i)) {
-                return i;
-            }
-            seen.insert(i);
+        for (int i = 0; i < nums.size(); i++) {
+            int curr = abs(nums[i]);
+            bool seen = nums[curr] < 1;
+            if (seen) return curr;
+            nums[curr] = -nums[curr];
         }
         return 0;
     }
