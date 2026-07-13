d72e302549e8435c8cc16923e6d3fbd6eea16733
Dhanush Ekollu <66331423+DEboy2007@users.noreply.github.com>
2026-07-12 22:26:06 -0400

    LC-rotting-oranges [Runtime: N/A; Memory: N/A]

--- matrix/rotting-oranges.cpp
+++ matrix/rotting-oranges.cpp
@@ -1,48 +1,46 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        ios::sync_with_stdio(false);
-        cin.tie(nullptr);
-        int m = grid.size();
-        int n = grid[0].size();
-        queue<pair<int,int>> q;
-        int fresh = 0;
-        // add all rotten oranges
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
+        deque<pair<int, int>> q;
+        vector<pair<int, int>> DIRS = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        bool min0 = true;
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[0].size(); j++) {
                 if (grid[i][j] == 2) {
-                    q.push({i,j});
-                } if (grid[i][j] == 1) {
-                    fresh++;
+                    q.push_back({i, j});
+                }
+                if (grid[i][j] == 1) {
+                    min0 = false;
                 }
             }
         }
-        vector<pair<int, int>> dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        if (min0) return 0;
         int mins = 0;
-        while (!q.empty() && fresh > 0) {
-            int size = q.size();
-            for (int i = 0; i < size; i++) {
-                auto [r, c] = q.front();
-                q.pop();
-                for (auto [dr, dc] : dirs) {
-                    int nr = r + dr;
-                    int nc = c + dc;
-                    if (nr < 0 || nc < 0 || nr >= m || nc >= n) {
-                        continue;
-                    } if (grid[nr][nc] != 1) {
-                        continue;
-                    } else {
-                        grid[nr][nc] = 2;
-                        q.push({nr, nc});
-                        fresh--;
-                    }
+        pair<int, int> lastMinEnd = q.back();
+        while (!q.empty()) {
+            pair<int, int> curr = q.front(); q.pop_front();
+            int i = curr.first;
+            int j = curr.second;
+            for (auto &[d1, d2] : DIRS) {
+                int x = i + d1;
+                int y = j + d2;
+                if (x < 0 || y < 0 || x >= grid.size() || y >= grid[0].size()) continue;
+                if (grid[x][y] != 1) continue;
+                grid[x][y] = 2;
+                q.push_back({x, y});
+            }
+            if (curr == lastMinEnd) {
+                mins++;
+                lastMinEnd = q.back();
+            }
+        }
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[0].size(); j++) {
+                if (grid[i][j] == 1) {
+                    return -1;
                 }
             }
-            mins++;
         }
-
-        if (fresh > 0) return -1;
-        return mins;
-        
+        return mins - 1;
     }
 };
