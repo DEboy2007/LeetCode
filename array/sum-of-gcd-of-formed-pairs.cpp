94dd4af9c40d99db8742c92134f7d2479539cd5c
Dhanush Ekollu <66331423+DEboy2007@users.noreply.github.com>
2026-07-15 23:17:54 -0400

    LC-sum-of-gcd-of-formed-pairs [Runtime: N/A; Memory: N/A]

--- array/sum-of-gcd-of-formed-pairs.cpp
+++ array/sum-of-gcd-of-formed-pairs.cpp
@@ -2,17 +2,16 @@ class Solution {
 public:
     long long gcdSum(vector<int>& nums) {
         int mx = nums[0];
-        vector<int> gcds(nums.size());
         for (int i = 0; i < nums.size(); i++) {
             mx = max(nums[i], mx);
-            gcds[i] = gcd(nums[i], mx);
+            nums[i] = gcd(nums[i], mx);
         }
-        sort(gcds.begin(), gcds.end());
+        sort(nums.begin(), nums.end());
         long long sum = 0;
         int i = 0;
-        int j = gcds.size() - 1;
+        int j = nums.size() - 1;
         while (i < j) {
-            sum += gcd(gcds[i], gcds[j]);
+            sum += gcd(nums[i], nums[j]);
             i++;
             j--;
         }
